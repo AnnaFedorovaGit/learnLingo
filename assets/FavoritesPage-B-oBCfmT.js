a95d960b1a812bf57906e652ebde1495febe1729
AnnaFedorovaGit <AnnaFedorovaGit@users.noreply.github.com>
2024-06-24 15:42:00 +0000

    deploy: efe9857acfa5c9884f672d3b16ba345cbd36a5b6

---
+++ assets/FavoritesPage-B-oBCfmT.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-7Gk9RGMx.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"FavoritesPage"})});export{s as default};
--- assets/FavoritesPage-Dj29aP-Y.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-B2M2xynf.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"FavoritesPage"})});export{s as default};
