6b1d0efc4bd37d8baa0067c73a8eb356f85486f0
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2024-07-04 17:47:36 +0300

    fix deploy error

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
 
-const instance = axios.create({
-    baseURL: "https://learnlingo-64672-default-rtdb.europe-west1.firebasedatabase.app/",
-})
+// const instance = axios.create({
+//     baseURL: "https://learnlingo-64672-default-rtdb.europe-west1.firebasedatabase.app/",
+// })
 
-export const requestAllTeachers = async () => { 
-    const { data } = await instance.get('/teachers');
-    return data;
-} 
+// export const requestAllTeachers = async () => { 
+//     const { data } = await instance.get('/teachers');
+//     return data;
+// } 
